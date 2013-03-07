e7183738888f9f367ac434a0fc1fcebb5e7b2c6a
Laxcor <Laxcor@78.91.0.52>
2013-03-07 14:54:08 +0100

    Client update

---
+++ MyWeekClient/src/gui/Login.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class Login extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
