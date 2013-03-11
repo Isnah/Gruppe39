f929b396a31d5bfea12ac2c9f54946b812efba65
Endre <Endre@78.91.20.55>
2013-03-11 14:33:05 +0100

    Client classes created

---
+++ MyWeekClient/src/client/Group.java
@@ -0,0 +1,75 @@
+package client;
+
+import java.util.ArrayList;
+
+
+
+
+public class Group {
+
+	/**
+	 * @Author Endre Elvestad
+	 */
+	final int id;
+	String name;
+	ArrayList<Person> member;
+	ArrayList<Group> subgroup;
+	
+	//Constructor
+	public Group(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	//Primary key
+	public int getId() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	//Returns all memebers in the current group - Not subgroups. 
+	public ArrayList<Person> getMember() {
+		return member;
+	}
+	
+	//Chekcs to see if a Person isntance is a memeber of the current group
+	public boolean isGroupMember(Person person) {
+		return member.contains(person);
+	}
+
+	
+	//add a new memeber to the current group
+	public boolean addMember(Person person) {
+		if(member.contains(person))return false;
+		member.add(person);
+		return true;
+	}
+
+	//Get a list of subgroups, where the current group is the parrent 
+	public ArrayList<Group> getSubgroup() {
+		return subgroup;
+	}
+
+	//Creates a new subgroup, where the current group is parrent. Returns False if allready a subgroup
+	public boolean addSubgroup(Group group) {
+		if(subgroup.contains(group)) return false;
+		subgroup.add(group);
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ MyWeekClient/src/client/Person.java
@@ -0,0 +1,74 @@
+package client;
+
+//import java.beans.PropertyChangeEvent;
+
+
+
+public class Person {
+
+	/**
+	 * @Author Endre Elvestad
+	 * 
+	 */
+	
+
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+
+	public Person(String email, String lastName, String firstName) {
+		this.email = email;
+		this.lastName = lastName;
+		this.firstName = firstName;
+	}
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+/**
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+		PropertyChangeEvent event = new PropertyChangeEvent(this, NAME_PROPERTY_NAME, oldName, name);
+		propChangeSupp.firePropertyChange(event);
+	}
+*/
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	/**
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+		PropertyChangeEvent event = new PropertyChangeEvent(this, NAME_PROPERTY_NAME, oldName, name);
+		propChangeSupp.firePropertyChange(event);
+	}
+
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+
+	/**
+	  
+	 public void setEmail(String email) {
+		this.email = email;
+		PropertyChangeEvent event = new PropertyChangeEvent(this, NAME_PROPERTY_NAME, oldName, name);
+		propChangeSupp.firePropertyChange(event);
+	}
+	
+	*/
+
+
+	
+	
+
+
+
+
+}
