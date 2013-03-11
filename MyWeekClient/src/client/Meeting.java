1d88413bb80f024ffb0ef0f0fbea700f17a00a18
Endre <Endre@78.91.11.54>
2013-03-11 18:53:54 +0100

    Minor changes in Person & Meeting

---
+++ MyWeekClient/src/client/Meeting.java
@@ -0,0 +1,63 @@
+package client;
+
+import java.sql.Time;
+import java.util.ArrayList;
+
+
+
+/**
+ * @author Hans Olav Slotte
+ */
+public class Meeting extends Appointment {
+	private ArrayList<Person> attendees;
+	private ArrayList<Group> groupAttendees;
+	
+	public Meeting(int id, Time start, Time end, String name, String descr, Person registeredBy, ArrayList<Person> initialAttendees, ArrayList<Group> initialGroups) {
+		super(id, start, end, name, descr, registeredBy);
+		attendees = initialAttendees;
+		groupAttendees = initialGroups;
+	}
+	
+	public Person getAttendee(int index) {
+		return attendees.get(index);
+	}
+	
+	public Group getGroupAttendee(int index) {
+		return groupAttendees.get(index);
+	}
+	
+	/**
+	 * Checks whether a person is already in the attendees list before adding
+	 * them to it.
+	 * @param person The person you want to add to the attendees list
+	 */
+	public void addAttendee(Person person) {
+		if(attendees.contains(person)) return;
+		attendees.add(person);
+	}
+	
+	/**
+	 * 
+	 * @return A copy of the attendee list. Person instances in the list are still
+	 * editable as normal.
+	 */
+	public ArrayList<Person> getAttendees() {
+		return new ArrayList<Person>(attendees);
+	}
+	
+	public boolean isAttending(Person person) {
+		return attendees.contains(person);
+	}
+	
+	/**
+	 * @return A copy of the group attendee list. Group instances are editable
+	 * as normal.
+	 */
+	public ArrayList<Group> getGroupAttendees() {
+		return new ArrayList<Group>(groupAttendees);
+	}
+	
+	public boolean isAttendingGroup(Group group) {
+		return groupAttendees.contains(group);
+	}
+}
--- MyWeekClient/src/client/Person.java
+++ MyWeekClient/src/client/Person.java
@@ -17,7 +17,6 @@ public class Person {
 
 	/**
 	 * @Author Endre Elvestad
-	 * 
 	 */  
 	
 
