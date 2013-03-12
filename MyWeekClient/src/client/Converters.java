9bb3337007c14a0e1d69c9e9923ddf651703ed51
Tobias Linkjendal <tobias@linkjendal.com>
2013-03-12 11:51:36 +0100

    RE

--- MyWeekClient/src/client/Converters.java
+++ MyWeekClient/src/client/Converters.java
@@ -17,13 +17,13 @@ public class Converters {
      * @param millis
      * @return 
      */
-    public static String MillisecondsToHHMM(long millis) {
-        String hhmmss = String.format("%02d:%02d", 
-            TimeUnit.MILLISECONDS.toHours(millis),
-            TimeUnit.MILLISECONDS.toMinutes(millis) - 
-            TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)));
-        return hhmmss;
-    }
+	public static String MillisecondsToHHMM(long millis) {
+		String hhmmss = String.format("%02d:%02d", 
+		TimeUnit.MILLISECONDS.toHours(millis),
+		TimeUnit.MILLISECONDS.toMinutes(millis) - 
+		TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)));
+		return hhmmss;
+	}
     
     public long dateAndTimeToMilliseconds(int year, int month, int day, int hour, int minutes){
     	Calendar c = c = Calendar.getInstance();
