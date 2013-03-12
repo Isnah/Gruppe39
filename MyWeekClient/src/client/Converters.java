c69942dfb3386d2c6e0884f2e9fdd5e762c26b7e
Laxcor <plaxcikus@gmail.com>
2013-03-12 11:31:01 +0100

    Added a general Converter class with a millisecond to HH:MM

---
+++ MyWeekClient/src/client/Converters.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author Laxcor
+ */
+public class Converters {
+    /**
+     * Converts milliseconds to a String HH:MM
+     * @param millis
+     * @return 
+     */
+    public String MillisecondsToHHMM(long millis) {
+        String hhmmss = String.format("%02d:%02d", 
+            TimeUnit.MILLISECONDS.toHours(millis),
+            TimeUnit.MILLISECONDS.toMinutes(millis) - 
+            TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)));
+        return hhmmss;
+    }
+}
