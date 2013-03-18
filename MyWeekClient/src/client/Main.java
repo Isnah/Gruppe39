b7625fe9b4f9558d4277c6aab8b225c0c4402cbf
tofo145 <tore.bjorlo@gmail.com>
2013-03-18 14:17:48 +0100

    communication logout

--- MyWeekClient/src/client/Main.java
+++ MyWeekClient/src/client/Main.java
@@ -103,7 +103,7 @@ public class Main {
      * Called by the main window to close all connections before disposing.
      */
     public void logout() {
-        ct.logout();
+        ct.logout(); 
     }
     
     public Meeting getMeeting(int id) {
